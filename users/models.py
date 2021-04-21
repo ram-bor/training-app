e1c5e34e11b60439a7e4f080882dfdabc6baa49f
RobertoRamos <robram91@gmail.com>
2021-04-20 21:36:37 -0400

    init forms
    :

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
 # import manager
@@ -8,9 +9,11 @@ from .managers import CustomUserManager
 
 # class CustomUser(models.Model):
 
-class CustomUser(AbstractUser):
-    username = None
+class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(_('email address'), unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
