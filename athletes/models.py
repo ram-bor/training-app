9b212ad285ea0f7285454ec8134c94ee05b7d937
RobertoRamos <robram91@gmail.com>
2021-04-22 01:52:56 -0400

    looks like Sport model is finito

--- athletes/models.py
+++ athletes/models.py
@@ -11,7 +11,24 @@ class Athlete(models.Model):
     run_longest_distance = models.DecimalField(max_digits=5, decimal_places=2)
 
 
-class Workout(models.Model):
+class Sport(models.Model):
+    RUN = 'R'
+    SWIM = 'S'
+    BIKE = 'B'
+    SPORT_CHOICES = [
+        (RUN, 'Run'),
+        (SWIM, 'Swim'),
+        (BIKE, 'Bike')
+    ]
+    sport_type = models.CharField(
+        max_length=32,
+        choices=SPORT_CHOICES,
+        default=RUN,
+    )
+
     athlete = models.ForeignKey(
         Athlete, on_delete=models.CASCADE, related_name='workouts')
     date = models.DateField()
+    planned_duration = models.TimeField(blank=True, null=True)
+    duration = models.TimeField()
+    hour_avg = models.TimeField()
