1572e251cd482c4d14e1b137748fa018ce4e65b0
RobertoRamos <robram91@gmail.com>
2021-04-21 22:35:19 -0400

    back to users, it works

--- users/views.py
+++ users/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
-
 from .forms import CustomUserCreationForm
 
 
+# Create your views here.
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
