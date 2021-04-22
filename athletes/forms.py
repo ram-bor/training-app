18e566bc8965e1ee4f7782d19c0e592904f4c411
RobertoRamos <robram91@gmail.com>
2021-04-22 01:12:48 -0400

    basic athlete model complete

--- athletes/forms.py
+++ athletes/forms.py
@@ -1,11 +1,13 @@
 from bootstrap_datepicker_plus import DatePickerInput
+from django import forms
 
-#  class ToDoForm(forms.Form):
-#         date = forms.DateField(
-#             widget=DatePickerInput(
-#                 options={
-#                     "format": "mm/dd/yyyy",
-#                     "autoclose": True
-#                 }
-#             )
-#         )
+
+class DateForm(forms.Form):
+    date = forms.DateField(
+        widget=DatePickerInput(
+            options={
+                "format": "mm/dd/yyyy",
+                "autoclose": True
+            }
+        )
+    )
--- athletes/models.py
+++ athletes/models.py
@@ -6,8 +6,12 @@ from django.db import models
 class Athlete(models.Model):
     first_name = models.CharField(max_length=40,)
     last_name = models.CharField(max_length=40,)
+    birth_date = models.DateField()
+    threshold_hr = models.IntegerField()
+    run_longest_distance = models.DecimalField(max_digits=5, decimal_places=2)
 
 
 class Workout(models.Model):
     athlete = models.ForeignKey(
         Athlete, on_delete=models.CASCADE, related_name='workouts')
+    date = models.DateField()
