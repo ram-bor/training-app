726e6fc9e080e6b7e5342f341d69f549a119be6a
RobertoRamos <robram91@gmail.com>
2021-04-25 23:47:50 -0400

    fixing class

--- athletes/views.py
+++ athletes/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from .forms import UserForm
+from django.views import View
 
 # Create your views here.
 
 
-def create_athlete(request):
-    athlete_form = UserForm()
-    return render(request, 'my_template.html', {'my_form': athlete_form})
+class AthleteCreate(View):
+    def create_athlete(request):
+        athlete_form = UserForm()
+        return render(request, 'athlete_form.html', {'my_form': athlete_form})
