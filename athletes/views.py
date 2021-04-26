90fd0ab577357082af37fcf9a621035cfc86fce8
RobertoRamos <robram91@gmail.com>
2021-04-26 16:22:07 -0400

    working on athlete forms

--- athletes/views.py
+++ athletes/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-from .forms import UserForm
+# from .forms import UserForm
+# from .forms import AthleteForm
 from django.views import View
 
 # Create your views here.
 
 
 class AthleteCreate(View):
+
     def get(self, request):
         athlete_form = UserForm()
         return render(request, 'athlete_form.html', {'form': athlete_form})
