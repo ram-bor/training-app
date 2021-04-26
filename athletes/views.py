4d359f20d485ad8adaccd00a0bef53991af4736a
RobertoRamos <robram91@gmail.com>
2021-04-25 23:42:25 -0400

    athlete view class or function

--- athletes/views.py
+++ athletes/views.py
@@ -4,6 +4,6 @@ from .forms import UserForm
 # Create your views here.
 
 
-# def create_user(request):
-#     user_form = UserForm()
-#     return render(request, 'my_template.html', {'my_form': user_form})
+def create_athlete(request):
+    athlete_form = UserForm()
+    return render(request, 'my_template.html', {'my_form': athlete_form})
