f02ef34d2171551d51b56ef6fa49bd2e607d2186
RobertoRamos <robram91@gmail.com>
2021-04-26 17:12:42 -0400

    fixed dir paths and secret key

--- training_project/settings.py
+++ training_project/settings.py
@@ -17,14 +17,14 @@ load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname...
+# BASE_DIR = os.path.dirname
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = str(os.getenv('SECRET_KEY'))
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
