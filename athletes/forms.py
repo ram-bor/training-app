a9f789fe71fd5718252bb18f067cedc078c10e59
RobertoRamos <robram91@gmail.com>
2021-04-26 21:22:53 -0400

    fixing

--- athletes/forms.py
+++ athletes/forms.py
@@ -1,15 +1,16 @@
 from bootstrap_datepicker_plus import DatePickerInput
 from django import forms
 
+
 class AthleteForm(forms.Form):
-    
+
 
 class DateForm(forms.Form):
     date = forms.DateField(
         widget=DatePickerInput(
             options={
                 "format": "mm/dd/yyyy",
-                "autoclose": True 
+                "autoclose": True
             }
         )
     )
