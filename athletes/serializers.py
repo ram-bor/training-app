07aec60360b626c259e9223d16b5cbbf33a7fe89
RobertoRamos <robram91@gmail.com>
2021-04-27 23:04:04 -0400

    serializing model data for future client use

--- athletes/serializers.py
+++ athletes/serializers.py
@@ -1,2 +1,14 @@
 from rest_framework import serializers
 from .models import Athlete, Training
+
+
+class AthleteSerializer(serializers.HyperlinkedModelSerializer):
+    training = serializers.HyperlinkedRelatedField(
+        view_name='athlete_info',
+        many=True,
+    )
+
+    class Meta:
+        model = Athlete
+        fields = ('id', 'first_name', 'last_name', 'birth_date',
+                  'threshold_hr', 'run_longest_distance', 'training')
