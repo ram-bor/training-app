7c7603010f4b10495629f6140caf1e3b354fd13f
RobertoRamos <robram91@gmail.com>
2021-04-28 22:43:11 -0400

    fixing urls

--- athletes/urls.py
+++ athletes/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    # path('', views.AthleteCreate.as_view(), name='athlete_create'),
+    path('athletes/new', views.AthleteCreate.as_view(), name='athlete_create'),
     path('athlete_info', views.AthleteInfo.as_view(), name='athlete_info'),
 ]
