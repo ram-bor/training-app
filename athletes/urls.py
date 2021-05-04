1bcef21da970bfd357d6328a935f3cef7c7bb173
RobertoRamos <robram91@gmail.com>
2021-05-03 22:20:53 -0400

    url bug

--- athletes/urls.py
+++ athletes/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('athletes/new', views.AthleteCreate.as_view(), name='athlete_create'),
-    path('athlete_info', views.athlete_info, name='athlete_info'),
+    path('athlete/<int:pk>', views.athlete_info, name='athlete_info'),
     path('training/new', views.TrainingCreate.as_view(), name='training_create'),
 
 ]
