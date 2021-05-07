656414a6d5314c0c53283315f3df6f5ae66865eb
RobertoRamos <robram91@gmail.com>
2021-05-07 00:44:15 -0400

    trouble in django

--- athletes/urls.py
+++ athletes/urls.py
@@ -4,8 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('athlete/new', views.AthleteCreate.as_view(), name='athlete_create'),
-    # path('athlete/<int:pk>', views.AthleteInfo.as_view(), name='athlete_info'),
-    path('athlete/some', views.AthleteInfo.as_view(), name='athlete_info'),
+    path('athlete/<int:pk>/', views.AthleteInfo.as_view()),
     path('training/new', views.TrainingCreate.as_view(), name='training_create'),
 
 ]
--- athletes/views.py
+++ athletes/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, ListAPIView
 from .models import Athlete, Training
 from .forms import AthleteForm, TrainingForm
 from rest_framework import generics, permissions
+from rest_framework.response import Response
 from .serializers import AthleteSerializer
 from django.urls import reverse_lazy
 
@@ -28,33 +29,34 @@ class AthleteCreate(View):
 
 
 # class AthleteInfo(generics.RetrieveUpdateDestroyAPIView):
-class AthleteInfo(View):
-    template_name = 'athlete_info.html'
-
-    def athlete_info(self, request, pk):
-        athlete = Athlete.objects.get(pk=3)
-        return render(request, self.template_name, athlete.pk)
 
     # queryset = Athlete.objects.get(pk=pk)
     # serializer_class = AthleteSerializer
     # permissions_classes = (permissions.IsAuthenticated)
 
+class AthleteInfo(APIView):
+    template_name = 'athlete_info.html'
 
-class TrainingCreate(View):
-    form_class = TrainingForm
-    template_name = 'training_form.html'
+    def get_athlete(self, request, pk):
+        athlete = Athlete.objects.get(pk=pk)
+        return Response()
 
-    def get(self, request):
-        form = self.form_class()
-        return render(request, self.template_name, {'form': form})
 
-    def post(self, request):
-        form = self.form_class(request.POST)
-        if form.is_valid():
-            training = form.save()
-            return redirect('training_info', pk=training.pk)
+# class TrainingCreate(View):
+#     form_class = TrainingForm
+#     template_name = 'training_form.html'
 
-        return render(request, self.template_name, {'form': form})
+#     def get(self, request):
+#         form = self.form_class()
+#         return render(request, self.template_name, {'form': form})
+
+#     def post(self, request):
+#         form = self.form_class(request.POST)
+#         if form.is_valid():
+#             training = form.save()
+#             return redirect('training_info', pk=training.pk)
+
+#         return render(request, self.template_name, {'form': form})
 
 
 # class TrainingDetail
