0416d79da73ae09884531e5a346f8920a4b8076a
RobertoRamos <robram91@gmail.com>
2021-05-07 01:24:30 -0400

    back to where i started for generic rest views, DRY code

--- athletes/views.py
+++ athletes/views.py
@@ -31,38 +31,38 @@ class AthleteCreate(View):
         return render(request, self.template_name, {'form': form})
 
 
-# class AthleteInfo(generics.RetrieveUpdateDestroyAPIView):
+class AthleteInfo(generics.RetrieveUpdateDestroyAPIView):
 
-    # queryset = Athlete.objects.get(pk=pk)
-    # serializer_class = AthleteSerializer
-    # permissions_classes = (permissions.IsAuthenticated)
+    queryset = Athlete.objects.all()
+    serializer_class = AthleteSerializer
+    permissions_classes = (permissions.IsAuthenticated)
 
-class AthleteInfo(APIView):
+# class AthleteInfo(APIView):
     # template_name = 'athlete_info.html'
 
-    def get_athlete(self, pk):
-        try:
-            return Athlete.objects.get(pk=pk)
-        except Athlete.DoesNotExist:
-            raise Http404
+    # def get_athlete(self, pk):
+    #     try:
+    #         return Athlete.objects.get(pk=pk)
+    #     except Athlete.DoesNotExist:
+    #         raise Http404
 
-    def get(self, request, pk, format=None):
-        athlete = self.get_athlete(pk)
-        serializer = AthleteSerializer(athlete)
-        return Response(serializer.data)
+    # def get(self, request, pk, format=None):
+    #     athlete = self.get_athlete(pk)
+    #     serializer = AthleteSerializer(athlete)
+    #     return Response(serializer.data)
 
-    def put(self, request, pk, format=None):
-        athlete = self.get_athlete(pk)
-        serializer = AthleteSerializer(athlete, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # def put(self, request, pk, format=None):
+    #     athlete = self.get_athlete(pk)
+    #     serializer = AthleteSerializer(athlete, data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, pk, format=None):
-        athlete = self.get_athlete(pk)
-        athlete.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    # def delete(self, request, pk, format=None):
+    #     athlete = self.get_athlete(pk)
+    #     athlete.delete()
+    #     return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class TrainingCreate(View):
