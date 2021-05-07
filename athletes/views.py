df730ac4f4d759b7fce634d779213ecd29f52152
RobertoRamos <robram91@gmail.com>
2021-05-07 01:17:55 -0400

    views are afixing

--- athletes/views.py
+++ athletes/views.py
@@ -5,8 +5,9 @@ from django.views import View
 from django.views.generic import ListView, DetailView
 from .models import Athlete, Training
 from .forms import AthleteForm, TrainingForm
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, Http404
 from rest_framework import generics, permissions
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
@@ -45,10 +46,23 @@ class AthleteInfo(APIView):
         except Athlete.DoesNotExist:
             raise Http404
 
-    def get(self, request, pk):
+    def get(self, request, pk, format=None):
         athlete = self.get_athlete(pk)
         serializer = AthleteSerializer(athlete)
         return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        athlete = self.get_athlete(pk)
+        serializer = AthleteSerializer(athlete, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        athlete = self.get_athlete(pk)
+        athlete.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class TrainingCreate(View):
